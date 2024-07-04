03aed345cbc6a64eca24f50dd99db4ddfd2e05e7
Valeria Giannelli <vg.giannellivaleria@gmail.com>
2024-07-04 15:00:15 +0200

    feat: create a title

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+// importo la componente che voglio
+import AppTitle from './components/AppTitle.vue';
+
+// dico cosa deve prendere da quella componente
+export default{
+  components: {
+    AppTitle
+  }
+}
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <!-- inserisco la componente nel template-->
+  <AppTitle/>
 </template>
 
 <style scoped>
